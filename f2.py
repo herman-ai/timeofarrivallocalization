4fa1aeefbf3bbe1c4a15ed49ad403a2529291246
HS <herman.sahota@gmail.com>
2014-11-24 18:19:36 -0800

    Computing standard deviation of x,y,z and errors.

--- f2.py
+++ f2.py
@@ -1,3 +1,7 @@
+"""
+Next step: Compute standard deviation of the estimates computed.
+"""
+
 from scipy.optimize import minimize
 from math import sqrt
 from random import random
@@ -9,11 +13,21 @@ from utils import *
 
 seed()
 
-#for epsilon_index in range(1):
-for epsilon_index in range(4):
-    print "********************************************"
-    print "EPSILON_INDEX = ", epsilon_index
-    print "********************************************"
+xyzAllActual = ()
+for i in range(NUM_SENSORS):
+    xyzAllActual = xyzAllActual + ((random() * F, random() * F, random() * (-H/40)), )
+
+epsilon_index = 1
+
+xEstSamples = [[] for _ in range(NUM_SENSORS)]
+yEstSamples = [[] for _ in range(NUM_SENSORS)]
+zEstSamples = [[] for _ in range(NUM_SENSORS)]
+errors = []
+
+for ctr in range(1000):
+    #print "********************************************"
+    #print "EPSILON_INDEX = ", epsilon_index
+    #print "********************************************"
     EPSILON_S_REAL = EPSILON_SOIL[epsilon_index]["real"]
     EPSILON_S_IMG = EPSILON_SOIL[epsilon_index]["img"]
 
@@ -30,11 +44,6 @@ for epsilon_index in range(4):
         ) \
     ) \
         ** (-1) * (10 ** (-7))
-
-
-    xyzAllActual = ()
-    for i in range(NUM_SENSORS):
-        xyzAllActual = xyzAllActual + ((random() * F, random() * F, random() * (-H/40)), )
 
 
     AnchorsXYZ = {"above": [(0., 0., H), (F, 0., H), (0., F, H), (F, F, H)], \
@@ -99,25 +108,25 @@ for epsilon_index in range(4):
         res = minimize(timeOfArrivalMatcherAnchorToOneSensor, xyzInitialEstimate, args=(i, obsTAnchors, AnchorsXYZ, observedTime3DSS, speed_soil), method='Nelder-Mead', \
         options = {"maxiter":1e6})
 
-    	e = res.x
-    	if (e[2] > 0 ):
-    	    e[2] = -e[2]
-        print res.success
+       	e = res.x
+       	if (e[2] > 0 ):
+       	    e[2] = -e[2]
+        #print res.success
         xyzFirstEst = xyzFirstEst  + ((e[0], e[1], e[2]),)
 
         #print xyzAllActual[i]
         #print e
-        print "*****"
-    print xyzAllActual
-    print "xyzFirstEst = ", xyzFirstEst
+        #print "*****"
+    #print xyzAllActual
+    #print "xyzFirstEst = ", xyzFirstEst
     res = minimize(timeOfArrialMatcher3DX, xyzFirstEst, args=(obsTAnchors, AnchorsXYZ, observedTime3DSS, speed_soil), method='Nelder-Mead', options = {"maxiter":1e6})
 
     e = [(res.x[i * 3: (i+1) * 3]) for i in range(NUM_SENSORS)]
 
-    for i in range(len(xyzAllActual)):
-        print "||||||"
-        print xyzAllActual[i]
-        print e[i]
+    #for i in range(len(xyzAllActual)):
+    #    print "||||||"
+    #    print xyzAllActual[i]
+    #    print e[i]
 
     params_text = r"\noindent$\epsilon_s'$ = " + '{0:.3g}'.format(EPSILON_S_REAL/EPSILON_0) + r"\\\\" \
     + r"$\epsilon_s''$ = " + '{0:.3g}'.format(EPSILON_S_IMG/EPSILON_0) + r"\\\\" \
@@ -144,11 +153,21 @@ for epsilon_index in range(4):
 
     #print "DELTA_T = ", DELTA_T, "error = ", err
     #print "T_S = ", T_S, "error = ", err
-    print "epsilon_index = ", epsilon_index, "error = ", err
+    print ctr, ". epsilon_index = ", epsilon_index, "error = ", err
+    errors.append(err)
+    for i in range(NUM_SENSORS):
+        xEstSamples[i].append(e[i][0])
+        yEstSamples[i].append(e[i][1])
+        zEstSamples[i].append(e[i][2])
 
-    plot([xyz[0] for xyz in xyzAllActual], [xyz[1] for xyz in xyzAllActual], \
-         [xyz[0] for xyz in xyzFirstEst], [xyz[1] for xyz in xyzFirstEst], \
-        [xyz[0] for xyz in e], [xyz[1] for xyz in e], EPSILON_S_REAL, EPSILON_S_IMG, params_text, "X", "Y", [-1, F + 1 , -1, F + 1])
-    plot([xyz[0] for xyz in xyzAllActual], [xyz[2] for xyz in xyzAllActual], \
-        [xyz[0] for xyz in xyzFirstEst], [xyz[2] for xyz in xyzFirstEst], \
-        [xyz[0] for xyz in e], [xyz[2] for xyz in e], EPSILON_S_REAL, EPSILON_S_IMG, params_text, "X", "Z", [-1, F + 1, -H/40, 0])
+    #plot([xyz[0] for xyz in xyzAllActual], [xyz[1] for xyz in xyzAllActual], \
+    #        [xyz[0] for xyz in xyzFirstEst], [xyz[1] for xyz in xyzFirstEst], \
+    #    [xyz[0] for xyz in e], [xyz[1] for xyz in e], EPSILON_S_REAL, EPSILON_S_IMG, params_text, "X", "Y", [-1, F + 1 , -1, F + 1])
+    #plot([xyz[0] for xyz in xyzAllActual], [xyz[2] for xyz in xyzAllActual], \
+    #    [xyz[0] for xyz in xyzFirstEst], [xyz[2] for xyz in xyzFirstEst], \
+    #    [xyz[0] for xyz in e], [xyz[2] for xyz in e], EPSILON_S_REAL, EPSILON_S_IMG, params_text, "X", "Z", [-1, F + 1, -H/40, 0])
+
+print "X STD = ", numpy.mean([numpy.std(x) for x in xEstSamples])
+print "Y STD = ", numpy.mean([numpy.std(x) for x in yEstSamples])
+print "Z STD = ", numpy.mean([numpy.std(x) for x in zEstSamples])
+print "error = ", numpy.mean(errors)
