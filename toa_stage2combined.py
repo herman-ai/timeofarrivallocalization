52c4f09fd3bde5f24fd8ddc62dc5bdbe179c497b
Herman Sahota <herman@cerebras.net>
2017-04-23 00:55:49 -0700

    Change to matrix algebra.

--- toa_stage2combined.py
+++ toa_stage2combined.py
@@ -5,6 +5,10 @@ NUM_SENSORS = 5
 
 scale_lst_sq_obj_fn = 10**23
 
+H = 200
+F = 100
+
+
 T_S = 10.0 ** (-3)
 PS_0 = 10  # 19 dBm
 MU_0 = 4 * pi * 10 ** (-7)
@@ -87,15 +91,15 @@ def toa_neg_log_likelihood_soil2soil(xyz, toa_observed_from_other_sensors_dir,
     #for direct path
     for i in range(xyz.shape[-2]):
         for j in range(xyz.shape[-2]):
-            if toa_observed_from_other_sensors_dir[i, j] > 0:
+            if i == j:
+                continue
+            if toa_observed_from_other_sensors_dir[i, j] > 0 and toa_observed_from_other_sensors_reflected[i,j] > 0:
                 dist = np.sqrt(np.sum((xyz[i] - xyz[j]) ** 2))
                 mean_toa = dist / speed_soil
                 sigma2_toa = sigma_soil2Soil(dist, ALPHA_SOIL)**2
-                neg_log_likelihood += ((toa_observed_from_other_sensors_dir[i, j] - mean_toa) ** 2) /sigma2_toa
-                neg_log_likelihood += 0.5*np.log(sigma2_toa)
+                # neg_log_likelihood += ((toa_observed_from_other_sensors_dir[i, j] - mean_toa) ** 2) /sigma2_toa
+                # neg_log_likelihood += 0.5*np.log(sigma2_toa)
 
-            if toa_observed_from_other_sensors_reflected[i,j] > 0:
-                # print("i={},j={}".format(i,j))
                 d1 = np.sqrt((xyz[i, 2]) ** 2 + \
                              ((xyz[i, 0] - xyz[j, 0]) ** 2 + \
                               (xyz[i, 1] - xyz[j, 1]) ** 2) *
@@ -107,10 +111,26 @@ def toa_neg_log_likelihood_soil2soil(xyz, toa_observed_from_other_sensors_dir,
                              ((xyz[j, 2] / (xyz[i, 2] + xyz[j, 2])) ** 2))
 
                 dist = d1 + d2
-                mean_toa =dist/speed_soil
-                sigma2_toa = sigma_soil2Soil_reflected(dist, ALPHA_SOIL) ** 2
-                neg_log_likelihood += ((toa_observed_from_other_sensors_reflected[i,j]-mean_toa)**2)/sigma2_toa
-                neg_log_likelihood += 0.5*np.log(sigma2_toa)
+                mean_toa_ref = dist / speed_soil
+                sigma2_toa_ref = sigma_soil2Soil_reflected(dist, ALPHA_SOIL) ** 2
+                # neg_log_likelihood += ((toa_observed_from_other_sensors_reflected[i, j] - mean_toa_ref) ** 2) / sigma2_toa_ref
+                # neg_log_likelihood += 0.5 * np.log(sigma2_toa_ref)
+
+                t = np.asarray([toa_observed_from_other_sensors_dir[i, j], toa_observed_from_other_sensors_reflected[i, j]]).reshape(2,1)
+                t_bar = np.asarray([mean_toa, mean_toa_ref])
+                sigma = np.asarray([[1/sigma2_toa[0],0], [0,1/sigma2_toa_ref[0]]])
+
+                a = sigma.dot(t-t_bar)
+                b = (t-t_bar).T
+                # neg_log_likelihood += ((t-t_bar).dot(sigma)).dot((t-t_bar).T)
+                neg_log_likelihood += b.dot(a)
+                neg_log_likelihood -= 0.5*np.log(np.linalg.det(sigma))
+            else:
+                raise Exception("test")
+
+            # if toa_observed_from_other_sensors_reflected[i,j] > 0:
+                # print("i={},j={}".format(i,j))
+
 
     return neg_log_likelihood
 
@@ -134,13 +154,15 @@ if __name__=="__main__":
 
             power = p_average_soil2soil(dist, ALPHA_SOIL)
 
-            print("power = {} dB".format(power))
+            # print("power = {} dB".format(power))
 
             if power > N_0:
                 mean_toa = dist / speed_soil
                 sigma_toa = sigma_soil2Soil(dist, ALPHA_SOIL=ALPHA_SOIL)
                 ob = np.random.normal(loc=mean_toa, scale=sigma_toa, size=1)[0]
                 toa_observed_from_other_sensors_dir[i,j] = ob
+            else:
+                print("****************power alert***********")
 
     print("observed toa from other sensors (dir)=\n{}".format(toa_observed_from_other_sensors_dir))
 
@@ -164,29 +186,37 @@ if __name__=="__main__":
 
             power = p_average_soil2soil_reflected(dist, ALPHA_SOIL)
 
-            print("power = {} dB".format(power))
+            # print("power = {} dB".format(power))
 
             if power > N_0:
                 mean_toa = dist / speed_soil
                 sigma_toa = sigma_soil2Soil_reflected(dist, ALPHA_SOIL=ALPHA_SOIL)
                 ob = np.random.normal(loc=mean_toa, scale=sigma_toa, size=1)[0]
                 toa_observed_from_other_sensors_reflected[i,j] = ob
+            else:
+                print("**************2.power alert***********")
     #
     print("observed toa from other sensors =\n{}".format(toa_observed_from_other_sensors_reflected))
 
+    # xyz0 = np.random.random_sample(size=(NUM_SENSORS, 3)) * (F, F, -H / H)  # X, Y, Z coordinates
+    xyz0 = est_s_locations_stage1
 
 
     sq_error_at_actual = toa_neg_log_likelihood_soil2soil(actual_s_locations*(1,1,z_scale),
                                                           toa_observed_from_other_sensors_dir,
                                                           toa_observed_from_other_sensors_reflected)
     print("squared error at actual = {}".format(sq_error_at_actual))
-    sq_error_at_seed = toa_neg_log_likelihood_soil2soil(est_s_locations_stage1*(1,1,z_scale),
+
+
+    sq_error_at_seed = toa_neg_log_likelihood_soil2soil(xyz0*(1,1,z_scale),
                                                            toa_observed_from_other_sensors_dir,
                                                         toa_observed_from_other_sensors_reflected)
     print("squared error at seed = {}".format(sq_error_at_seed))
+
     bnds = (((None, None),)*2+((-100,0),))*NUM_SENSORS
+
     result = minimize(toa_neg_log_likelihood_soil2soil,
-                      est_s_locations_stage1*(1,1,z_scale),
+                      xyz0*(1,1,z_scale),
                       args=(toa_observed_from_other_sensors_dir,
                             toa_observed_from_other_sensors_reflected),
                       method="L-BFGS-B",
@@ -195,7 +225,7 @@ if __name__=="__main__":
     print(result.success)
     est_s_locations_stage2a = result.x.reshape(-1, 3) * (1, 1, 1 / z_scale)
     print("actual =\n{}".format(actual_s_locations))
-    print("stage 1=\n{}".format(est_s_locations_stage1))
+    # print("stage 1=\n{}".format(est_s_locations_stage1))
     print("stage 2 =\n{}".format(est_s_locations_stage2a))
     np.savetxt("data/est_stage2a.csv", est_s_locations_stage2a)
 
